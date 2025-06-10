5505251523855f2cf67b99bb58116a4abdfc981f
Leohayequipo <lic.mannanice@gmail.com>
2025-06-10 18:20:50 -0300

    Primer commir

---
+++ formulario.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Traductor de Letras al Chino</title>
+</head>
+<body>
+    <h1>Traductor Español ➜ Chino</h1>
+
+    <form action="traducir.php" method="POST">
+        <label for="letra">Ingresá una letra (a-z):</label>
+        <input type="text" name="letra" id="letra" maxlength="1" required>
+        <button type="submit">Traducir</button>
+    </form>
+</body>
+</html>
