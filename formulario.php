b6c271c1a9787aa0c6271315b5b93c6f228fb76f
Timoteo Sayago <timodiscord.x@gmail.com>
2025-06-10 20:17:53 -0300

    fix

--- formulario.php
+++ formulario.php
@@ -3,14 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <title>Traductor de Letras al Chino</title>
+    <link rel="stylesheet" href="estilo.css">
 </head>
 <body>
-    <h1>Traductor Español ➜ Chino</h1>
-
     <form action="traducir.php" method="POST">
+        <h1>Traductor Español ➜ Chino</h1>
         <label for="letra">Ingresá una letra (a-z):</label>
         <input type="text" name="letra" id="letra" maxlength="1" required>
         <button type="submit">Traducir</button>
     </form>
 </body>
 </html>
+ 
